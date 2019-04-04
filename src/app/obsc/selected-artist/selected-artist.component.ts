379feceea4fb42a1ef40300c186cde082d79ab13
MorleyWebDev <johnmorley13@gmail.com>
2019-04-04 18:07:04 +0100

    added remove artist functionality

--- src/app/obsc/selected-artist/selected-artist.component.ts
+++ src/app/obsc/selected-artist/selected-artist.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ArtistModel } from '../model/artist.model';
+import { ArtistService } from '../service/artist.service';
 
 @Component({
   selector: 'app-selected-artist',
@@ -10,7 +11,11 @@ export class SelectedArtistComponent implements OnInit {
 
   @Input() chosenArtists: ArtistModel[] = [];
 
-  constructor() { }
+  constructor(private artService: ArtistService) { }
+
+  removeSelection(i){
+    this.artService.chosenArtists.splice(i, 1);
+  }
 
   ngOnInit() {
     
